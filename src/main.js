2293cbafdc75133cf726ac93f1836fd49d9bc7e9
Fyodor <shobshob481@gmail.com>
2024-10-02 18:30:10 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "../node_modules/modern-normalize/modern-normalize.css";
+import "./styles/style.css";
+import "./styles/header.css";
+import "./styles/utils.css";
+import "./styles/hero.css";
+import "./styles/about.css";
+import "./styles/work.css";
