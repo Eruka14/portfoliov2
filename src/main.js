f0e9a9a4cdabce3022a781d3e24083d2d1eafea1
Fyodor <shobshob481@gmail.com>
2024-10-07 01:25:15 +0800

    some structure change

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import "../node_modules/modern-normalize/modern-normalize.css";
 import "./styles/style.css";
 import "./styles/header.css";
-import "./styles/utils.css";
 import "./styles/hero.css";
 import "./styles/about.css";
 import "./styles/work.css";
 import "./styles/contact.css";
 import "./styles/footer.css";
 import "./styles/mobilemenu.css";
+import "./styles/utils.css";
 
 import mobileNav from "./utils/mobile-nav";
 import darkMode from "./utils/dark-mode";
